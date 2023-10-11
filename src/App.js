e0cbd7f9021a246934fdd0606c98e33279b0041c
Tomillionaire <aderonmutomilola@gmail.com>
2023-10-11 06:29:51 -0700

    react-shopping-cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react'
+import Navbar from './components/Navbar';
 
-function App() {
+import Amazon from './components/Amazon';
+import Cart from './components/Cart';
+import './styles/amazon.css'
+
+
+
+const App = () => {
+  const [show,setShow] = useState(true);
+  const [cart,setCart] = useState([]);
+  const [warning,setWarning] = useState(false);
+
+  const handleClick = (item) =>{
+    let isPresent = false;
+    cart.forEach((product) =>{
+      if (item.id === product.id)
+      isPresent = true;
+    });
+    if (isPresent) {
+        setWarning(true);
+        setTimeout(()=>{
+          setWarning(false);
+        }, 2000);
+        return;
+    } 
+    setCart([...cart, item]);
+  }
+
+  const handleChange = (item, d) =>{
+   let ind = -1;
+   cart.forEach((data, index)=>{
+    if (data.id === item.id)
+        ind = index;
+   });
+
+   const tempArr = cart;
+   tempArr[ind].amount += d;
+   if (tempArr[ind].amount === 0)
+   
+     tempArr[ind].amount = 1;
+    setCart([...tempArr])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <div>
+      <Navbar size={cart.length} setShow={setShow} />
+      {
+        show ?  <Amazon handleClick={handleClick}/> :   <Cart cart={cart} setCart={setCart} handleChange={handleChange}/>
+      }
+     
+    
+      {
+        warning && <div className='warning'>Item is already added to your cart.</div>
+      }
+     
+     </div>
+    
+
+  
+  )
 }
 
-export default App;
+export default App
